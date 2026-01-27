013584a9046ad6bc95f42366cd7f90914aefc899
pra9ptl <pranavpatel1409@gmail.com>
2026-01-26 19:08:58 -0600

    update ui

--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box } from "@mui/joy";
 import Booking from "../components/Booking";
 
 const BookingPage: React.FC = () => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
+
   return (
     <Box className="full-width-bg" id="booking-page">
       <Booking />
